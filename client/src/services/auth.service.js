b3b05e79756df475e17b33c54c81e59547a2b0b5
David Harris <jdh.programmer@gmail.com>
2021-01-20 15:30:55 -0600

    writing axious requests for register and login pages

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,2 +1,9 @@
 import axios from "axios";
 
+const API_URL = (username, email, password) => {
+    return axios.post(API_URL + "signup", {
+        username,
+        email,
+        password,
+    });
+};
