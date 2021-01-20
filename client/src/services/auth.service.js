0d502bb039ad86c4986ec3716f5211306017095b
David Harris <jdh.programmer@gmail.com>
2021-01-20 15:31:51 -0600

    login and logout added

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -7,3 +7,26 @@ const API_URL = (username, email, password) => {
         password,
     });
 };
+
+const login = (username, password) => {
+    return axios.post(API_URL + "signin", {
+        username,
+        password,
+    })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data
+        });
+};
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+export default {
+    register,
+    login,
+    logout,
+};
