75e09d362a1ba56b1170abd8d690584bf5cf8d61
David Harris <jdh.programmer@gmail.com>
2021-01-20 22:18:45 -0600

    register.js validators

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,50 @@
 import React, {useState, useRef} from "react";
 import {useDispatch, useSelector} from "react-redux";
 
-import Form from "react-validation/build/form"
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import {isEmail} from "validator";
+
+import {register} from "../actions/auth";
+
+const required = (value) => {
+    if (!value) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This field is required!
+            </div>
+        );
+    }
+};
+
+const validEmail = (value) => {
+    if (!isEmail(value)) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                This is not a valid email.
+            </div>
+        );
+    }
+};
+
+const vusername = (value) => {
+    if (value.length < 3 || value.length > 20) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                The username must be between 3 and 20 characters.
+            </div>
+        );
+    }
+};
+
+const vpassword = (value) => {
+    if (value.length < 6 || value.length > 40) {
+        return (
+            <div className="alert alert-danger" role="alert">
+                The password must be between 6 and 40 characters.
+            </div>
+        );
+    }
+};
+
